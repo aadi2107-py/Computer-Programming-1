f30c2ee117d913a558d52a0a981a312d51070ebc
aadi2107-py <aadidave21@gmail.com>
2025-08-08 20:40:33 +0530

    subtraction.c

--- subtraction.c
+++ subtraction.c
@@ -3,15 +3,16 @@ int main()
 {
     int a,b;
     float c;
-    printf("Enter the 1st number");
+    printf("Enter the 1st number:");
     scanf("%d",&a);
 
-    printf("Enter the 2nd number");
+    printf("Enter the 2nd number:");
     scanf("%d",&b);
         
     c = a - b;
     printf("%d - %d = %f\n",a,b,c);
     return 0;
 }
+
 
 
