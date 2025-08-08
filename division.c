915c96a1afe9d0cf0831773b8c6be1d748423c90
aadi2107-py <aadidave21@gmail.com>
2025-08-08 19:34:12 +0530

    division.c

--- division.c
+++ division.c
@@ -3,14 +3,15 @@ int main()
 {
     int a;
     float b,c;
-    printf("Enter a number:");
+    printf("Enter the 1st number:");
     scanf("%d",&a);
 
-    printf("Enter a number:");
+    printf("Enter the 2nd number:");
     scanf("%f",&b);
     
     c = a / b;
     printf("%d / %f = %f\n",a,b,c);
     return 0;
 }
+
 
