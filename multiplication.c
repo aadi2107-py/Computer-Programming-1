4abc822e12ce4d1f41ff6f5ec9e52d129f0573da
aadi2107-py <aadidave21@gmail.com>
2025-08-08 19:28:13 +0530

    Multiplication.c

--- multiplication.c
+++ multiplication.c
@@ -2,9 +2,14 @@
 int main()
 {
     int a,b, c;
-    printf("Enter two numbers:");
-    scanf("%d %d",&a,&b);
+    printf("Enter a number:");
+    scanf("%d",&a);
+
+    printf("Enter a number");
+    scanf("%d",&b);
+    
     c = a * b;
     printf("%d * %d = %d\n",a,b,c);
     return 0;
 }
+
