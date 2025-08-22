409487d11aa0e2b3ad3cb0626e8af90b0ca79013
aadi2107-py <aadidave21@gmail.com>
2025-08-22 12:19:48 +0530

    Largest and Smallest.c

--- Largest and Smallest.c
+++ Largest and Smallest.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<math.h>
 int main ()
 {
     int a,b,c;
@@ -41,40 +42,45 @@ int main ()
         printf("Highest is %d\n",c);
         printf("Lowest is %d\n",b);
     }
-    else if (a=b && a>c)
+    else if (a==b && a>c)
     {
         printf("1st and 2nd number are equal to each other %d=%d\n",a,b);
         printf("Lowest is %d\n",c);
     }
-    else if (a=b && a<c)
+    else if (a==b && a<c)
     {
         printf("1st and 2nd number are equal to each other %d=%d\n",a,b);
         printf("Highest is %d\n",c);
     }
-    else if(b=c && b>a)
+    else if(b==c && b>a)
     {
         printf("2nd and 3rd number are equal to each other %d=%d\n",b,c);
         printf("Lowest is %d\n",a);
     }
-    else if (b=c && b<a)
+    else if (b==c && b<a)
     {
         printf("2nd and 3rd number are equal to each other %d=%d\n",b,c);
         printf("Highest is %d\n",a);
     }
-    else if (a=c && a>b)
+    else if (a==c && a>b)
     {
         printf("1st and 3rd number are equal to each other %d=%d\n",a,c);
         printf("Lowest is %d\n",b);
     }
-    else if (a=c && a<b)
+    else if (a==c && a<b)
     {
         printf("1st and 3rd number are equal to each other %d=%d\n",a,c);
         printf("Highest is %d\n",b);
     }
+    else if (a==b && b==c)
+    {
+        printf("All three numbers are equal %d = %d = %d\n",a,b,c);
+    }
     else
     {
-        printf("All three numbers are equal- %d = %d = %d\n",a,b,c);
+        printf("No possible outcome");
     }
+
 
     return 0;
 
