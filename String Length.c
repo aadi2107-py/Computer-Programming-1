3883c0e2e8310eed16b56ee4feb8fc69d4f5ecb5
aadi2107-py <aadidave21@gmail.com>
2025-10-20 01:03:59 +0530

    Add files via upload

---
+++ String Length.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+
+{
+    char str[100];
+    int Length = 0;
+
+    printf("Please enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    while (str[Length] != '\0')
+    {
+        Length++;
+    }
+    
+    printf("The Length of the string is: %d\n", Length);
+    
+    return 0;
+}
